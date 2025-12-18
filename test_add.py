467f12ed0dea5306821f59f0c2c4307db6d74e86
Mute <1515587869@qq.com>
2025-12-18 21:39:58 +0800

    first commit

---
+++ test_add.py
@@ -0,0 +1,21 @@
+def add(x,y):
+    return x + y
+    
+
+def test_add():
+    assert add(1,10) == 11
+    assert add(1,1) == 2
+    assert add(1,99) == 100
+
+class TestClass:
+    def test_one(self):
+        x = "this"
+        assert "h" in x
+        print("h" in x)
+
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, "check")
+
+test_obj = TestClass()
+test_obj.test_one()
